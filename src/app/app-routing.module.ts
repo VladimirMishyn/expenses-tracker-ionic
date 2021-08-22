1f6c729085d6d83aa52993f20cbc5d41790d63c8
Volodymyr Mishyn <v.mishyn@corp.semantrum.net>
2021-08-23 01:29:21 +0300

    -removing boilerplate code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: '/expenses',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'authorize',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule)
   }
 ];
 
